2d0b123a1becc815426ca89e3bbaea9cf449e0fb
Humoyun Toshmatov <tosmatovhumoun03@gmail.com>
2025-05-09 11:24:46 +0500

    ..

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
 from http.server import BaseHTTPRequestHandler
+from ddos import send_request
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        result = send_request()  # вызов функции из test1.py
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(result.encode('utf-8'))
         return
